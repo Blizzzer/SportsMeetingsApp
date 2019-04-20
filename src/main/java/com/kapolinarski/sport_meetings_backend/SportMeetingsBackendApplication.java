c5f7b08ae655c6445fc97488090bab212e9e5343
Krzysztof Apolinarski <k.apolinarski@gmail.com>
2019-04-20 19:51:27 +0200

    Initial Commit

---
+++ src/main/java/com/kapolinarski/sport_meetings_backend/SportMeetingsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kapolinarski.sport_meetings_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportMeetingsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportMeetingsBackendApplication.class, args);
+    }
+
+}
